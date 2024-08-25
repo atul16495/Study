4bda7e59d3fc83c6733e0ecb2000390847deccff
Atul <atuldafale16495@gmail.com>
2024-08-25 23:15:46 +0530

    Added example of testing razer views

---
+++ ViewExample/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View(); //Views/Home/Index.cshtml
+            //return View("abc"); //Home.cshtml
+           // return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}
