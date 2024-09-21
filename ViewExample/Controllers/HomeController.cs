1c881be3645da8a39f2c4314cc01bae490dbb8b6
Atul <atuldafale16495@gmail.com>
2024-09-21 21:13:38 +0530

    updated view bag code

--- ViewExample/Controllers/HomeController.cs
+++ ViewExample/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ViewExample.Models;
 
 namespace ViewExample.Controllers
 {
@@ -8,6 +9,14 @@ namespace ViewExample.Controllers
         [Route("/")]
         public IActionResult Index()
         {
+            ViewData["appTitle"] = "Asp.Net Core Demo App";
+            List<Person> people = new List<Person>()
+             {
+             new Person(){Name="Atul",DateOfBirth=DateTime.Parse("2002-05-06"),PersonGender = Gender.Male},
+             new Person() { Name = "jon", DateOfBirth = DateTime.Parse("2005-09-06"), PersonGender = Gender.Female },
+             new Person(){Name="boss",DateOfBirth=DateTime.Parse("2006-05-20"),PersonGender = Gender.other}
+             };
+            ViewData["people"]=people;
             return View(); //Views/Home/Index.cshtml
             //return View("abc"); //Home.cshtml
            // return new ViewResult() { ViewName = "abc" };
