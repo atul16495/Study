e494062ab6fae52c316370f30c9f98b7e052ea2a
Atul <atuldafale16495@gmail.com>
2024-11-10 14:55:46 +0530

    updated repo for partial views

--- PartialViewsExamples/Controllers/HomeController.cs
+++ PartialViewsExamples/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewsExamples.Models;
 
 namespace PartialViewsExamples.Controllers
 {
@@ -7,13 +8,26 @@ namespace PartialViewsExamples.Controllers
         [Route("/")]
         public IActionResult Index()
         {
-            return View();
+           return View();
         }
 
         [Route("about")]
         public IActionResult About()
         {
             return View();
+        }
+
+        [Route("programming-languages")]
+        public IActionResult ProgrammingLanguages()
+        {
+            ListModel listModel = new ListModel()
+            {
+                ListTitle = "Programming Languages List",
+                ListItems = new List<string> {"python","C#","Go"}
+            };
+            return PartialView("_ListPartialView", listModel);
+           // return new PartialViewResult() { ViewName = "_ListPartialView",Model = listModel }; 
+
         }
     }
 }
