0f552f281a0a87f5a0a190ed4f0a445331807ae8
Atul Dafale <atuldafale16@outlook.com>
2025-03-22 22:44:04 +0530

    added Dependency injextioon example

---
+++ DIExample/Services/CitiesService.cs
@@ -0,0 +1,22 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "London",
+                "Paries",
+                "Mumbai",
+                "Pune"
+            };
+        }
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+    }
+}
